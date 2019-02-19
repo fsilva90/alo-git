1dd22fdd6c40bcf51223db562719475143a3dc1c
Felipe Silva <silva1990@gmail.com>
2019-02-18 22:35:16 -0300

    Comandos

---
+++ ComandosGit.sql
@@ -0,0 +1,38 @@
+Comandos GIT
+
+mkdir alo-git -- Criar pasta
+
+cd alo-git -- Escolher a pasta
+
+git init 	-- Iniciar GIT
+
+git status  -- Verificar os arquivos que não foram add ou alterados
+
+git add . -- adiciona todos os arquivos
+
+git ls-files -- lista os arquivos na pasta
+
+git commit  -m "" -- Commita os arquivos
+
+git branch -- verificar qual o branch em uso
+
+git checkout -b nomebranch   --riar branch e usar ao mesmo tempo
+
+git log -- Verificar o log 
+
+git remote -v -- Logar no GIT
+
+.gitignore -- Ignorar arquivos
+
+git remote add origin https://github.com/fsilva90/alo-git.git -- Adiciona os arquivos no repositório GIT 
+
+git push origin master -- Upload dos arquivo, após logado
+
+git checkout master -- Trocar para branch master
+
+git merge layout -- migrar dados da branch layout para branch master
+
+git push origin master -- migrar dados após o merge para branch master
+
+git pull origin master -- Baixar os arquivos do GITHUB para o GIT
+
